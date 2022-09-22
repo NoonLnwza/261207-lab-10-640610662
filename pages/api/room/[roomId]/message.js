bb79fe5dba8e543c86a02d030d7b0274ef6971f8
NoonLnwza <pubet50@gmail.com>
2022-09-22 16:12:25 +0700

    completed

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,41 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const id = req.query.roomId;
+    const roomIdIDX = rooms.findIndex((x) => x.roomId === id);
+
+    if (roomIdIDX === -1)
+      return res.status(404).json({
+        ok: false,
+        message: "Invalid room id",
+      });
+    const messages = rooms[roomIdIDX].messages;
+    return res.json({
+      ok: true,
+      messages,
+    });
   } else if (req.method === "POST") {
     const rooms = readDB();
-
+    const id = req.query.roomId;
+    const roomIdIDX = rooms.findIndex((x) => x.roomId === id);
     //read request body
     const text = req.body.text;
-
     //create new id
     const newId = uuidv4();
+    if (roomIdIDX === -1)
+      return res.status(404).json({
+        ok: false,
+        message: "Invalid room id",
+      });
+    if (typeof text !== "string") {
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
+    } else {
+      const newtext = {
+        messageId: newId,
+        text: text,
+      };
+      writeDB(rooms);
+      return res.json({ ok: true, message: newtext });
+    }
   }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -2,6 +2,27 @@ import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
+  const rooms = readDB();
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    const roomIdIDX = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdIDX === -1)
+      return res.status(404).json({
+        ok: false,
+        message: "Invalid room id",
+      });
+    const mess = rooms[roomIdIDX].messages;
+    const messageIdIDX = mess.findIndex((x) => x.messageId === messageId);
+    if (messageIdIDX === -1) {
+      return res.status(404).json({
+        ok: false,
+        message: "Invalid message id",
+      });
+    } else {
+      mess.splice(messageIdIDX, 1);
+      writeDB(rooms);
+      return res.json({ ok: true });
+    }
+  }
 }
